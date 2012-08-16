3405d7dc5cd4021f643a5377adb06542c30a6a91
rmbaur <rmb62@cornell.edu>
2012-08-16 12:47:30 -0400

    Add preprocessors to ProcPane layouts

--- titan/frontend/procpane.py
+++ titan/frontend/procpane.py
@@ -1,6 +1,7 @@
 from PyQt4 import QtCore, QtGui
 
 from .ui import ui_procpane
+from ..analysis.processors import Rescaler, BkgSubtractor
 
 
 class ProcPane(ui_procpane.Ui_Form, QtGui.QWidget):
@@ -9,3 +10,25 @@ class ProcPane(ui_procpane.Ui_Form, QtGui.QWidget):
 		super(ProcPane, self).__init__(parent)
 
 		self.setupUi(self)
+
+		self.preprocs = [Rescaler(), BkgSubtractor()]
+
+		self.prebox_items = [proc.shortdesc for proc in self.preprocs]
+		self.preOptions.addItems(self.prebox_items)
+
+		self.preStack = QtGui.QStackedWidget()
+		layout = QtGui.QVBoxLayout()
+		layout.addWidget(self.preStack)
+		for proc in self.preprocs:
+			self.preStack.addWidget(proc.widget)
+
+		print self.preStack.count()
+
+		self.current_preproc = self.preprocs[0]
+		self.preStackBox.setLayout(layout)
+		self.preStack.setCurrentWidget(self.current_preproc.widget)
+		self.preOptions.currentIndexChanged.connect(self.preStack.setCurrentIndex)
+		self.preOptions.currentIndexChanged.connect(self.set_current_preproc)
+
+	def set_current_preproc(self, idx):
+		self.current_preproc = self.preprocs[idx]
