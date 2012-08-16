b6dc8e7bc0b4c589afa7909ed302fc2efd10b225
rmbaur <rmb62@cornell.edu>
2012-08-15 21:46:00 -0400

    Allow import of images into HDF5 files

--- titan/frontend/mainwindow.py
+++ titan/frontend/mainwindow.py
@@ -2,14 +2,20 @@ import sys
 import os
 
 from PyQt4 import QtCore, QtGui
+from libtiff import TIFF as tiff
+import numpy as np
+
 from praxes.frontend.phynx import FileModel, FileView
 from praxes.io.phynx.migration.spec import convert_to_phynx
+from praxes.io.phynx.measurement import Measurement
 
 from .ui import ui_mainwindow
 from .plotpane import ImshowCanvas, PlotCanvas
 
 
 class MainWindow(ui_mainwindow.Ui_MainWindow, QtGui.QMainWindow):
+
+	proxy_changed = QtCore.pyqtSignal()
 
 	def __init__(self, parent=None):
 		super(MainWindow, self).__init__(parent)
@@ -26,6 +32,13 @@ class MainWindow(ui_mainwindow.Ui_MainWindow, QtGui.QMainWindow):
 
 		self.verticallayout1.addWidget(self.twod_viewer)
 		self.verticallayout1.addWidget(self.oned_viewer)
+
+		self.proxy = None
+		self.fileView.clicked.connect(self.get_proxy_from_index)
+
+	def get_proxy_from_index(self, index):
+		self.proxy = self.fileModel.getProxyFromIndex(index).getNode()
+		self.proxy_changed.emit()
 
 	@QtCore.pyqtSignature("")
 	# Slimmed down version from Praxes
@@ -48,6 +61,33 @@ class MainWindow(ui_mainwindow.Ui_MainWindow, QtGui.QMainWindow):
 				h5file = convert_to_phynx(str(filename), h5_filename=str(h5_filename))
 				h5file.close()
 				self.fileModel.openFile(str(h5_filename))
+
+
+	@QtCore.pyqtSignature("")
+	def on_actionImportImages_triggered(self):
+		if not isinstance(self.proxy, Measurement):
+			confirm_import = QtGui.QMessageBox()
+			confirm_import.setText("The selected parent group is not a Measurement.")
+			confirm_import.setInformativeText("Do you want to import images anyway?")
+			confirm_import.setStandardButtons(QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
+			confirm_import.setDefaultButton(QtGui.QMessageBox.No)
+			choice = confirm_import.exec_()
+			if choice == QtGui.QMessageBox.No:
+				return
+
+		filenames = QtGui.QFileDialog.getOpenFileNames(
+						self,
+						"Select images to import",
+						os.getcwd(),
+						"TIF images (*.tif *.tiff)"
+						)
+
+		images = []
+
+		for name in sorted(filenames):
+			images.append(tiff.open(str(name), mode='r').read_image())
+
+		self.proxy.create_dataset("images", data=np.array(images))
 
 
 	@QtCore.pyqtSignature("")  # Magic that prevents double signal-emits
