ff2889f42a151af70bc5922b36fe65c27a79c971
rmbaur <rmb62@cornell.edu>
2012-08-16 14:51:55 -0400

    Do ProcBoxes manually to reduce code duplication

--- titan/frontend/procpane.py
+++ titan/frontend/procpane.py
@@ -1,34 +1,61 @@
 from PyQt4 import QtCore, QtGui
 
 from .ui import ui_procpane
-from ..analysis.processors import Rescaler, BkgSubtractor
+from ..analysis.processors import *
 
 
-class ProcPane(ui_procpane.Ui_Form, QtGui.QWidget):
+class ProcPane(QtGui.QWidget):
 
-	def __init__(self, parent=None):
-		super(ProcPane, self).__init__(parent)
-
-		self.setupUi(self)
+	def __init__(self):
+		super(ProcPane, self).__init__()
 
 		self.preprocs = [Rescaler(), BkgSubtractor()]
+		self.prebox = ProcBox('Preprocessing', self.preprocs)
 
-		self.prebox_items = [proc.shortdesc for proc in self.preprocs]
-		self.preOptions.addItems(self.prebox_items)
+		self.fitprocs = [FFTFitter(), PCAFitter(), GPSAFitter()]
+		self.fitbox = ProcBox('Fitting', self.fitprocs)
 
-		self.preStack = QtGui.QStackedWidget()
+		self.postprocs = [Normalizer()]
+		self.postbox = ProcBox('Postprocessing', self.postprocs)
+
 		layout = QtGui.QVBoxLayout()
-		layout.addWidget(self.preStack)
-		for proc in self.preprocs:
-			self.preStack.addWidget(proc.widget)
+		layout.addWidget(self.prebox)
+		layout.addWidget(self.fitbox)
+		layout.addWidget(self.postbox)
+		self.setLayout(layout)
 
-		print self.preStack.count()
 
-		self.current_preproc = self.preprocs[0]
-		self.preStackBox.setLayout(layout)
-		self.preStack.setCurrentWidget(self.current_preproc.widget)
-		self.preOptions.currentIndexChanged.connect(self.preStack.setCurrentIndex)
-		self.preOptions.currentIndexChanged.connect(self.set_current_preproc)
+class ProcBox(QtGui.QGroupBox):
 
-	def set_current_preproc(self, idx):
-		self.current_preproc = self.preprocs[idx]
+	def __init__(self, title, procs):
+		super(ProcBox, self).__init__()
+
+		self.setTitle(title)
+
+		self.procs = procs
+		items = [proc.shortdesc for proc in self.procs]
+		self.current_proc = procs[0]
+
+		self.stack = QtGui.QStackedWidget()
+		for proc in procs:
+			self.stack.addWidget(proc.widget)
+
+		self.options = QtGui.QComboBox()
+		self.options.addItems(items)
+
+		self.go_button = QtGui.QPushButton("Go")
+
+		self.options.currentIndexChanged.connect(self.stack.setCurrentIndex)
+		self.options.currentIndexChanged.connect(self.set_current_proc)
+		self.go_button.clicked.connect(self.current_proc.compute)
+
+		layout = QtGui.QVBoxLayout()
+		layout.addWidget(self.options)
+		layout.addWidget(self.stack)
+		layout.addWidget(self.go_button)
+		self.setLayout(layout)
+
+	def set_current_proc(self, idx):
+		self.go_button.clicked.disconnect(self.current_proc.compute)
+		self.current_proc = self.procs[idx]
+		self.go_button.clicked.connect(self.current_proc.compute)
